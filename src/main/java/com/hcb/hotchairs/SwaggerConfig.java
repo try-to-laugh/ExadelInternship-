e1060a512c03abc26a651ea84563bcb2b8b160a6
Nikita Davidovski <nikita.davidouski@gmail.com>
2020-07-17 13:12:16 +0300

    fixing swagger and creating user request

--- src/main/java/com/hcb/hotchairs/SwaggerConfig.java
+++ src/main/java/com/hcb/hotchairs/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package com.hcb.hotchairs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableSwagger2
+@Configuration
 public class SwaggerConfig {
+    @Primary
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
 }
--- src/main/java/com/hcb/hotchairs/controllers/UserController.java
+++ src/main/java/com/hcb/hotchairs/controllers/UserController.java
@@ -5,6 +5,9 @@ import com.hcb.hotchairs.dtos.UserDTO;
 import com.hcb.hotchairs.entities.User;
 import com.hcb.hotchairs.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/users")
@@ -29,10 +35,51 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getById(@PathVariable("id") Long id) {
+    public ResponseEntity<EntityModel<UserDTO>> getById(@PathVariable("id") Long id) {
+
         User user = userService.getById(id);
+
+        EntityModel<UserDTO> entityModel;
+
+        if(user.getRoles().contains("admin")){
+
+            entityModel= EntityModel.of(userConverter.toDTO(user),
+                    linkTo(methodOn(UserController.class).getById(userConverter.toDTO(user).getId())).withSelfRel(),
+                    new Link("ссыль", "My Bookings"),
+                    new Link("ссыль", "Staff Bookings"),
+                    new Link("ссыль", "Manage Offices"),
+                    new Link("ссыль", "Manage Roles"));
+        }else if(user.getRoles().contains("HR")){
+
+            entityModel= EntityModel.of(userConverter.toDTO(user),
+                    linkTo(methodOn(UserController.class).getById(userConverter.toDTO(user).getId())).withSelfRel(),
+                    new Link("ссыль", "My Bookings"),
+                    new Link("ссыль", "Staff Bookings"),
+                    new Link("none", "Manage Offices"),
+                    new Link("none", "Manage Roles"));
+        }else if(user.getRoles().contains("editor")){
+
+            entityModel= EntityModel.of(userConverter.toDTO(user),
+                    linkTo(methodOn(UserController.class).getById(userConverter.toDTO(user).getId())).withSelfRel(),
+                    new Link("ссыль", "My Bookings"),
+                    new Link("none", "Staff Bookings"),
+                    new Link("ссыль", "Manage Offices"),
+                    new Link("none", "Manage Roles"));
+        }else {
+
+            entityModel = EntityModel.of(userConverter.toDTO(user),
+                    linkTo(methodOn(UserController.class).getById(userConverter.toDTO(user).getId())).withSelfRel(),
+                    new Link("ссыль", "My Bookings"),
+                    new Link("none", "Staff Bookings"),
+                    new Link("none", "Manage Offices"),
+                    new Link("none", "Manage Roles"));
+        }
+
         return Objects.isNull(user) ?
-                ResponseEntity.notFound().build() : ResponseEntity.ok(userConverter.toDTO(user));
+                ResponseEntity.notFound().build() :
+                ResponseEntity //
+                        .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+                        .body(entityModel);
     }
 
     @GetMapping("/")
