2da669ace5d7b6860ff6d20d84ecb9a4aaf0ce3f
Zitrasa <zitrasa@yandex.ru>
2020-07-19 16:41:24 +0300

    Hotfix csrf disabling to test login.

--- src/main/java/com/hcb/hotchairs/configs/SecurityConfig.java
+++ src/main/java/com/hcb/hotchairs/configs/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/test/**", "/restore/**").permitAll()
                     .anyRequest().authenticated()
