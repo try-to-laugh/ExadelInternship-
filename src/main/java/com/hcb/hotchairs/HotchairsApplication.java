7170ba13cdd01fb4dd238c04aac3e137b890e688
Никита Давидовский <nikita.davidouski@gmail.com>
2020-07-20 13:52:57 +0000

    HotchairsApplication.java edited

--- src/main/java/com/hcb/hotchairs/HotchairsApplication.java
+++ src/main/java/com/hcb/hotchairs/HotchairsApplication.java
@@ -1,24 +1,13 @@
 package com.hcb.hotchairs;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class HotchairsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotchairsApplication.class, args);
-	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration().setSkipNullEnabled(true);
-
-		return modelMapper;
 	}
 }
