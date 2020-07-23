16bbbc555e6c5d53fc19f08b44985eab8c4bb6d4
Ivan Kushner <ivan.kushner73@gmail.com>
2020-07-23 10:43:57 +0300

    PlaceRequestDTO

---
+++ src/main/java/com/hcb/hotchairs/dtos/PlaceRequestDTO.java
@@ -0,0 +1,17 @@
+package com.hcb.hotchairs.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class PlaceRequestDTO {
+    private Long officeId;
+    private Long floorId;
+    private Date date;
+    List<Long> tagsId = new ArrayList<>();
+}
