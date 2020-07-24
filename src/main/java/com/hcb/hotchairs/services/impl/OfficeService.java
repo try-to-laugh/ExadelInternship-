87935f838914eb0a4e577c3a459a5e861c581ea6
Zitrasa <zitrasa@yandex.ru>
2020-07-24 14:21:09 +0300

    Implemented extended office information sorting and pagination.

--- src/main/java/com/hcb/hotchairs/services/impl/OfficeService.java
+++ src/main/java/com/hcb/hotchairs/services/impl/OfficeService.java
@@ -6,12 +6,15 @@ import com.hcb.hotchairs.dtos.OfficeDTO;
 import com.hcb.hotchairs.entities.Office;
 import com.hcb.hotchairs.services.IOfficeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,18 +24,18 @@ public class OfficeService implements IOfficeService {
     private final OfficeConverter officeConverter;
 
     @Autowired
-    public OfficeService(IOfficeDAO officeDAO, OfficeConverter officeConverter){
+    public OfficeService(IOfficeDAO officeDAO, OfficeConverter officeConverter) {
         this.officeDAO = officeDAO;
         this.officeConverter = officeConverter;
     }
 
     @Override
-    public List<OfficeDTO> getAll(){
+    public List<OfficeDTO> getAll() {
         return officeDAO.findAll().stream().map(officeConverter::toDTO).collect(Collectors.toList());
     }
 
     @Override
-    public List<OfficeDTO> getAllByCityId(Long cityId){
+    public List<OfficeDTO> getAllByCityId(Long cityId) {
         return officeDAO.findAllByCityId(cityId).stream().map(officeConverter::toDTO).collect(Collectors.toList());
     }
 
@@ -46,5 +49,29 @@ public class OfficeService implements IOfficeService {
     @Modifying
     public OfficeDTO saveOffice(OfficeDTO newOffice) {
         return officeConverter.toDTO(officeDAO.save(officeConverter.fromDTO(newOffice)));
+    }
+
+    @Override
+    public List<OfficeDTO> getPagedAndSorted(Integer pageNumber, Integer pageSize, String sortMethod) {
+        Sort sortConfig;
+
+        switch (sortMethod) {
+            case "city":
+                sortConfig = Sort.by(Sort.Direction.ASC, "City.name");
+                break;
+
+            case "country":
+                sortConfig = Sort.by(Sort.Direction.ASC, "City.Country.name");
+                break;
+
+            default:
+                sortConfig = Sort.by(Sort.Direction.DESC, "Id");
+                break;
+        }
+
+        Pageable pageConfig = PageRequest.of(pageNumber, pageSize, sortConfig);
+        Page<Office> officePage = officeDAO.findAll(pageConfig);
+
+        return officePage.get().map(officeConverter::toDTO).collect(Collectors.toList());
     }
 }
