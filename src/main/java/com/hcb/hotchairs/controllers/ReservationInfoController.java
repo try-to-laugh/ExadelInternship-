665547af4a9a2a661c35f607164f8835fdfa9e87
Ivan Kushner <ivan.kushner73@gmail.com>
2020-07-28 00:17:33 +0300

    another typo

--- src/main/java/com/hcb/hotchairs/controllers/ReservationInfoController.java
+++ src/main/java/com/hcb/hotchairs/controllers/ReservationInfoController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/reservationInfo")
+@RequestMapping("/api/reservationInfo")
 public class ReservationInfoController {
 
     private final IReservationInfoService reservationInfoService;
